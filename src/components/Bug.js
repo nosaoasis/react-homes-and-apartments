dad9ceb2a2f010cfaf1f6f8099a2cf1a9594df75
Nosa <nagho@icitsolutions.com>
2021-06-05 20:50:07 +0100

    deleted the test files

--- src/components/Bug.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Bug extends Component {
-  render() {
-    return <div>We got a bug but we fixed it.....</div>;
-  }
-}
--- src/components/UndeleteTwo.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UndeleteTwo() {
-  return <div>the undeleting coponet</div>;
-}
