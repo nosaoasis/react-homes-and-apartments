ad1806eb3eb8b795eae4e22efce84067a9a9251e
Nosa <nagho@icitsolutions.com>
2021-06-05 13:35:19 +0100

    added the undelete component

---
+++ src/components/Undelete.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Undelete extends Component {
+  render() {
+    return (
+      <>
+        <div>hello from the undelete component</div>
+      </>
+    );
+  }
+}
+
+export default Undelete;
